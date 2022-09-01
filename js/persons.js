292c1b5ef6777447abbf5e3f58a5b4a30c3b802b
Deanna <deanna@softwarebastards.nl>
2022-09-01 22:44:58 +0200

    feat: add test validator scheme

--- js/persons.js
+++ js/persons.js
@@ -1,23 +1,33 @@
+// const personSchema = {
+//     name: 'string',
+//     age: 'number',
+//     siblings: 'array',
+//     metaData: 'object',
+//     active: 'boolean'
+//  }
+
 const personSchema = {
-    name: 'string',
-    age: 'number',
-    siblings: 'array',
-    metaData: 'object',
-    active: 'boolean',
- };
+  name: (value) => {
+    if (typeof value == "string") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
 
 // Validates true
 const personObj = {
-    name: 'James',
-    age: 25,
-    siblings: ['Johnnathan'],
-    metaData: {},
-    active: true,
+  name: "James",
+  age: 25,
+  siblings: ["Johnnathan"],
+  metaData: {},
+  active: true
 };
 
 // Validates false
 const personObjF = {
-    name: 'James',
-    age: 25,
-    active: true,
+  name: "James",
+  age: 25,
+  active: true
 };
